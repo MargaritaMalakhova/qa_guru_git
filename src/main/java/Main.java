a1203aaf69306e917496b128a4895fba57a2a6b9
MargaritaMalakhova <70913116+MargaritaMalakhova@users.noreply.github.com>
2023-04-04 14:24:22 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa guru!");
+        String helloMessage = "Hello qa guru!";
+        System.out.println(helloMessage);
     }
 }
